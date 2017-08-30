c2c33d80cfa7d49b7b9560fe934966b03d0c403a
David FletcherE <dtsfletcher@gmail.com>
2017-08-30 14:17:02 -0400

    cat works

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -8,7 +8,10 @@ end
 def contest
 end
 
-
+def kitten
+requested_size = params[:size]
+@kitten_url = "http://lorempixel.com/#{requested_size}/#{requested_size}/cats"
+end
 
 
 
--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,7 @@ Rails.application.routes.draw do
 get '/' => 'pages#welcome'
 get '/welcome' => 'pages#welcome'
 get '/contest'=> 'pages#contest'
+get '/kitten/:size' => 'pages#kitten'
 
 
 
